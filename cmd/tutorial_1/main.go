ba135c67637211026dd1c8c79a46e3e845c79b5c
brandon_L_ohaver <brandon.ohaver@gmail.com>
2023-12-23 17:32:35 -0600

    Messing around with Arrays, Slices, Maps, and Loops in Go

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,37 +1,42 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
-	var printValue string = "Hello World!"
-	printMe(printValue)
+	intArr := [...]int32{1,2,3} // [0, 0, 0]
+	fmt.Println(intArr)
 
-	var numerator int = 11
-	var denominator int = 2
-	var result, remainder, err = intDivision(numerator, denominator)
-	if err != nil {
-		fmt.Printf(err.Error())
-	} else if remainder == 0 {
-		fmt.Printf("The result of the integer division is %v\n", result)
+	var intSlice []int32 = []int32{4,5,6}
+	fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
+	intSlice = append(intSlice, 7)
+	fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
+
+	var intSlice2 []int32 = []int32{8,9}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Println(intSlice)
+
+	var intSlice3 []int32 = make([]int32, 3, 8)
+	fmt.Println(intSlice3)
+
+	var myMap map[string]uint8 = make(map[string]uint8)
+	fmt.Println(myMap)
+
+	var myMap2 = map[string]uint8 {"Adam":23, "Sarah":45}
+	fmt.Println(myMap2["Adam"])
+	var age, ok = myMap2["Jason"]
+	if ok {
+		fmt.Printf("The age is %v\n", age)
 	} else {
-		fmt.Printf("The result of the integer division is %v with remainder %v\n", result, remainder)
+		fmt.Printf("This profile is not valid!\n")
 	}
-}
 
-func printMe(printValue string) {
-	fmt.Println(printValue)
-}
-
-func intDivision(numerator int, denominator int) (int, int, error) {
-	var err error
-	if denominator == 0 {
-		err = errors.New("Cannot Divide by Zero")
-		return 0, 0, err
+	for name, age := range myMap2 {
+		fmt.Printf("Name: %v, Age: %v\n", name, age)
 	}
-	var result int = numerator/denominator
-	var remainder int = numerator%denominator
-	return result, remainder, err
+
+	for i, v := range intArr {
+		fmt.Printf("Index: %v, Value: %v\n", i, v)
+	}
 }
