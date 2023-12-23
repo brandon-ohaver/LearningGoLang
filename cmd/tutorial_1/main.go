8be26e2c251ea075fbd397b8bb8a0f564175896f
brandon_L_ohaver <brandon.ohaver@gmail.com>
2023-12-23 15:44:10 -0600

    Learned basic syntax, variables and data types

---
+++ cmd/tutorial_1/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intNum int = 32767
+	intNum = intNum + 1
+	fmt.Println(intNum)
+
+	var floatNum float64 = 12345678.9
+	fmt.Println(floatNum)
+
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+	var result float32 = floatNum32 + float32(intNum32)
+	fmt.Println(result)
+
+	var intNum1 int = 3
+	var intNum2 int = 2
+	fmt.Println(intNum1/intNum2)
+
+	var myString string = "Hello World"
+	fmt.Println(myString)
+
+	var myRune = 'a'
+	fmt.Println(myRune)
+
+	var myBoolean bool = false
+	fmt.Println(myBoolean)
+
+	var intNum3 rune
+	fmt.Println(intNum3)
+
+	myVar  := "string value"
+	fmt.Println(myVar)
+
+	var1, var2 := 1, 2
+	fmt.Println(var1, var2)
+
+	const myConst string = "const value"
+	fmt.Println(myConst)
+
+	const pi float32 = 3.1415
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module LearningGoLang
+
+go 1.21.5
