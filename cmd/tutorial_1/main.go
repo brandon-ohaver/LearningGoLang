1ffdd39a3c4e26c76a7425eb23d276dc6b307895
brandon_L_ohaver <brandon.ohaver@gmail.com>
2023-12-23 20:17:35 -0600

    Messing around with Go routines in Go

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,19 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+) 
+
+var m = sync.RWMutex{}
+var wg = sync.WaitGroup{}
+var dbData = []string {"id1", "id2", "id3", "id4", "id5"}
+var results = []string {}
 
 func main() {
-	var thing1 = [5]float64{1, 2, 3, 4, 5}
-	fmt.Printf("\nThe memory location of the thing1 array is: %p", &thing1)
-	var result [5]float64 = square(&thing1)
-	fmt.Printf("\nThe result is: %v", result)
-	fmt.Printf("\nThe value of thing1 is: %v", thing1)
+	t0 := time.Now()
+	for i := 0; i < len(dbData); i++ {
+		wg.Add(1)
+		go dbCall(i)
+	}
+	wg.Wait()
+	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
+	fmt.Printf("\nThe results are %v", results)
 }
 
-func square(thing2 *[5]float64) [5]float64 {
-	fmt.Printf("\nThe memory location of the thing2 array is: %p", thing2)
-	for i := range thing2 {
-		thing2[i] = thing2[i] * thing2[i]
-	}
-	return *thing2
+func dbCall (i int) {
+	// simulate DB call delay
+	var delay float32 = 2000
+	time.Sleep(time.Duration(delay)*time.Millisecond)
+	save(dbData[i])
+	log()
+	wg.Done()
+}
+
+func save (result string) {
+	m.Lock()
+	results = append(results, result)
+	m.Unlock()
+}
+
+func log() {
+	m.RLock()
+	fmt.Printf("\nThe current results are: %v", results)
+	m.RUnlock()
 }
