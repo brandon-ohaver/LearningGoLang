9dd60ca52ea514505ddc54e93ec8152c3656bd0c
brandon_L_ohaver <brandon.ohaver@gmail.com>
2023-12-23 18:55:33 -0600

    Messing around with strings, runes and bytes in Go

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -2,41 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	intArr := [...]int32{1,2,3} // [0, 0, 0]
-	fmt.Println(intArr)
-
-	var intSlice []int32 = []int32{4,5,6}
-	fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
-	intSlice = append(intSlice, 7)
-	fmt.Printf("The length is %v with capacity %v\n", len(intSlice), cap(intSlice))
-
-	var intSlice2 []int32 = []int32{8,9}
-	intSlice = append(intSlice, intSlice2...)
-	fmt.Println(intSlice)
-
-	var intSlice3 []int32 = make([]int32, 3, 8)
-	fmt.Println(intSlice3)
-
-	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println(myMap)
-
-	var myMap2 = map[string]uint8 {"Adam":23, "Sarah":45}
-	fmt.Println(myMap2["Adam"])
-	var age, ok = myMap2["Jason"]
-	if ok {
-		fmt.Printf("The age is %v\n", age)
-	} else {
-		fmt.Printf("This profile is not valid!\n")
+	var myString = []rune("résumé")
+	var indexed = myString[1]
+	fmt.Printf("%v, %T\n", indexed, indexed)
+	for i, v := range myString {
+		fmt.Println(i, v)
 	}
+	fmt.Printf("\nThe length of 'myString' is %v\n", len(myString))
 
-	for name, age := range myMap2 {
-		fmt.Printf("Name: %v, Age: %v\n", name, age)
-	}
+	var myRune = 'a'
+	fmt.Printf("myRune = %v\n", myRune)
 
-	for i, v := range intArr {
-		fmt.Printf("Index: %v, Value: %v\n", i, v)
+	var strSlice = []string {"b", "r", "a", "n", "d" , "o", "n"}
+	var strBuilder strings.Builder
+	for i := range strSlice {
+		strBuilder.WriteString(strSlice[i])
 	}
+	var catStr = strBuilder.String()
+	fmt.Printf("%v\n", catStr)
 }
